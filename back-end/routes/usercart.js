72eae8c1edd378a39b0370d720dcd5542b50150f
Nikhil Arora <narora200@gmail.com>
2017-06-30 23:03:02 +0530

    #2 Issue resolved: Route for incrementing quantity

--- back-end/routes/usercart.js
+++ back-end/routes/usercart.js
@@ -20,7 +20,17 @@ router.get('/:id/cart', (req, res)=>{
 })
 
 router.put('/:userid/:productid/increment', (req, res)=>{
-	
+	Cart.find({
+		        where: {
+		            productId: req.params.productid, 
+		            userId: req.params.userid
+		        }
+    		}).then((model)=>{
+		    	console.log('Inside the model');
+		        //Incrementing the value of quantity column by 1
+		        res.send('Success');
+		        return model.increment({"quantity": 1});
+	    	});
 })
 
 module.exports = router;
