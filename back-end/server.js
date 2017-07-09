349e914107212cca6562a831b1fcfe338419a629
Nikhil Arora <narora200@gmail.com>
2017-07-09 12:26:04 +0530

    Back-end: CORS added to provide the resources of **back-end** to **front-end**

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = require('./routes'); 
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 app.use('/', router);
 app.listen(3333, ()=>{
 	console.log('Listening on port 3333');
